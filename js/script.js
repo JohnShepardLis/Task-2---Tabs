baa3c723dd0ab579e9a098d42fe2c7c9b6acbc4e
John <johnshepardlisa@gmail.com>
2019-10-15 12:18:09 +0800

    Task-2: Tabs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
 
+    
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
